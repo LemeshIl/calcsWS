8112df64650ed3a9b72010f7b181af616400f29e
LemeshIl <artadoksa@gmail.com>
2019-12-10 11:52:22 +0400

    классы заглушки

--- src/ru/study/client/CalcWebServiceClient.java
+++ src/ru/study/client/CalcWebServiceClient.java
@@ -9,7 +9,7 @@ public class CalcWebServiceClient {
         CalcWebServiceImplService calcWebServiceImplService = new CalcWebServiceImplService();
         CalcWebService calcWebService = calcWebServiceImplService.getCalcWebServiceImplPort();
         System.out.println(calcWebService.getCalcAdd(12, 2));
-        System.out.println(calcWebService.getCalcMultiply(40, 2));
+        System.out.println(calcWebService.getCalcMultiply(20, 2));
         System.out.println(calcWebService.getCalcSubtract(40, 2));
         System.out.println(calcWebService.getCalcDivide(40, 2));
     }
